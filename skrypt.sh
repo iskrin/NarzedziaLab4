bf1a3de396b8e30c8eb0896e839b31c7d52431a0
Wiktor <dsw51748@student.dsw.edu.pl>
2024-05-20 22:44:18 +0200

    dodano obsługę parametru w --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,27 +1,26 @@
-if [ "$1" == "--date" ]; then
-<<<<<<< HEAD
-  echo "Dzisiejsza data to: $(date +"%Y-%m-%d")"
-else
-  echo "podaj flage"
-fi
-=======
-    echo "Dzisiejsza data to: $(date +"%Y-%m-%d")"
-elif [ "$1" == "--logs" ]; then
+function createFiles() {
     script_name=$(basename "$0")
+    num_files=$1
 
-# Tworzenie 100 plików logx.txt
-for i in {1..100} 
-do
+    # Tworzenie plików logx.txt
+    for ((i=1; i<=num_files; i++))
+    do
     file_name="log${i}.txt"
-    current_date=$(date +"%Y-%m-%d")
 
+    current_date=$(date +"%Y-%m-%d")
     echo "File name: $file_name" > "$file_name"
     echo "Script name: $script_name" >> "$file_name"
     echo "Date: $current_date" >> "$file_name"
-done
+    done
+}
 
+if [ "$1" == "--date" ]; then
+    echo "Dzisiejsza data to: $(date +"%Y-%m-%d")"
+elif [ "$1" == "--logs" ] && [ -z "$2" ]; then
+    createFiles 100
+elif [ "$1" = "--logs" ] && [ -n "$2" ]; then
+    createFiles $2
 else
   echo "podaj flage"
 fi
 
->>>>>>> logBrnach
